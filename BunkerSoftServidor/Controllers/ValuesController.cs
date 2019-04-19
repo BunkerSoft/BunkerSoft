a1e69f26c1cdbab6ad21abb59b296f877013b0ea
BunkerSoft <viral_haker@hotmail.com>
2019-04-18 19:42:27 -0500

    asp core 3

---
+++ BunkerSoftServidor/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace BunkerSoftServidor.Controllers
+{
+
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class ValuesController: Controller
+    {
+    private readonly IApplicationRepository  _repository;
+    
+        public ValuesController(IApplicationRepository repository)
+        {
+            _repository = repository;
+        }
+  
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            //var listado=_repository.FindAllAsync().ToList();
+            return new string[] { "a", "b", "c", "Luis" };
+            //return OK(listado);
+        }
+
+    
+    }
+
+ 
+}
---
+++ BunkerSoftServidor/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using BunkerSoftServidor.Models;
+
+namespace BunkerSoftServidor.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IApplicationRepository  _repository;
+
+        public IndexModel(IApplicationRepository repository) => _repository = repository;
+        public async Task OnGetAsync()
+        {
+            await _repository.Query(Pruebas);
+        }
+        public void OnGet()
+        {
+
+        }
+    }
+}
